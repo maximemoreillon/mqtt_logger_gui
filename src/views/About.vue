931230eea684f934334dd191ae469a0a5b5a03bb
Maxime Moreillon <m.moreillon@gmail.com>
2022-07-09 05:23:38 +0000

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-card>
+    <v-card-title>
+      MQTT Logger
+    </v-card-title>
+    <v-card-text>
+      Developed and maintained by Maxime Moreillon
+    </v-card-text>
+    <v-card-text>
+      Version {{ version }}
+    </v-card-text>
+    <v-card-text>
+      API URL: {{ api_url }}
+    </v-card-text>
+  </v-card>
 </template>
+<script>
+
+const {
+  VUE_APP_MQTT_LOGGER_API_URL
+} = process.env
+
+import {version} from '@/../package.json'
+
+export default {
+  name: 'About',
+  data(){
+    return {
+      api_url: VUE_APP_MQTT_LOGGER_API_URL,
+      version
+    }
+  }
+}
+
+</script>
